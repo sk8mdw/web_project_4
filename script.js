383bc96f606d957d1d0c598aaaa87dc360f68db1
Mark Worsham <mworsham@gmail.com>
2021-10-20 19:52:52 -0500

    feat: working on initial JS setup

---
+++ script.js
@@ -0,0 +1,17 @@
+const editFormEl = document.querySelector(`.popup__form`)
+
+const popupEl = document.querySelector(".popup")
+
+const editButton = document.querySelector(`.profile__edit-button`)
+
+const closeButton = document.querySelector(`.button_close`)
+
+const nameInput = document.querySelector("#name")
+
+const titleInput = document.querySelector("#title")
+
+const name = document.querySelector(`.popup__input_name`)
+
+const title = document.querySelector(`.profile__description`)
+
+console.log(editFormEl);
