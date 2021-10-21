eb499183d35e264141ef6fc7c244ca39aa317afc
Mark Worsham <mworsham@gmail.com>
2021-10-21 16:47:52 -0500

    style: javascript cleanup

--- script.js
+++ script.js
@@ -1,41 +1,35 @@
 let popupEl = document.querySelector(".popup");
 let editFormEl = document.querySelector(".popup__form");
-
 let editButton = document.querySelector(".profile__edit-button");
 let closeButton = document.querySelector(".button_close");
-
 let nameInput = document.querySelector(".popup__input_name");
 let titleInput = document.querySelector(".popup__input_title");
-
 let name = document.querySelector(".profile__name");
 let title = document.querySelector(".profile__title");
 
-function handleFormOpen() {
+function formOpen() {
   popupEl.classList.add("popup_opened");
-
-    nameInput.value = "";
-    titleInput.value = "";
+  nameInput.value = "";
+  titleInput.value = "";
 }
 
-function handleFormClose() {
+function formClose() {
   popupEl.classList.remove("popup_opened");
 }
 
-function handleFormSubmit(evt) {
+function formSubmit(evt) {
   evt.preventDefault();
-
   nameInputValue = nameInput.value;
   titleInputValue = titleInput.value;
 
   name.textContent = nameInputValue;
   title.textContent = titleInputValue;
 
-      handleFormClose();
-    
+  formClose();
 }
 
-editButton.addEventListener("click", handleFormOpen);
+editButton.addEventListener("click", formOpen);
 
-closeButton.addEventListener("click", handleFormClose);
+closeButton.addEventListener("click", formClose);
 
-editFormEl.addEventListener("submit", handleFormSubmit);
+editFormEl.addEventListener("submit", formSubmit);
