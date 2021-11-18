da110750fe49d361035a2ce5a50fe978d6abab86
Mark Worsham <mworsham@gmail.com>
2021-11-18 11:24:33 -0600

    feat: starting project 5

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,30 @@
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg"
+  }
+];
+
 let popupEl = document.querySelector(".popup");
 let editFormEl = document.querySelector(".popup__form");
 let editButton = document.querySelector(".profile__edit-button");
@@ -31,3 +58,7 @@ editButton.addEventListener("click", formOpen);
 closeButton.addEventListener("click", formClose);
 
 editFormEl.addEventListener("submit", formSubmit);
+
+initialCards.forEach(function(card) {
+  console.log(card);
+})
