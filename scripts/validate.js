30396e02b0511af79cfedf9d3f8e3b3f07b2c6ac
Mark Worsham <mworsham@gmail.com>
2021-12-15 14:59:22 -0600

    feat: inital html additions and validation js

---
+++ scripts/validate.js
@@ -0,0 +1,67 @@
+const isValid = (inputEl) => inputEl.validity.valid;
+
+const checkInputValidity = (formEl, inputEl, settings) => {
+	const errorEl = formEl.querySelector(`#${inputEl.id}-error`);
+    
+	if (!isValid(inputEl)) {
+        inputEl.classList.add(settings.inputErrorClass);
+		// console.log(formEl);
+		// console.log(inputEl);
+		// console.log(errorEl);
+		errorEl.textContent = inputEl.validationMessage;
+		errorEl.classList.add(settings.errorClass);
+	} else {
+		inputEl.classList.remove(settings.inputErrorClass);
+		// console.log(inputEl);
+		// console.log(errorEl);
+		errorEl.textContent = " ";
+		errorEl.classList.remove(settings.errorClass);
+	}
+};
+
+toggleButtonState = (inputList, buttonEl, { inactiveButtonClass }) => {
+	const allValid = inputList.every((inputEl) => isValid(inputEl));
+	if (allValid) {
+		buttonEl.classList.remove(inactiveButtonClass);
+		buttonEl.disabled = false;
+	} else {
+		buttonEl.classList.add(inactiveButtonClass);
+		buttonEl.disabled = true;
+	}
+};
+
+const setupEventListeners = (
+	formEl,
+	{ inputSelector, submitButtonSelector, ...otherSettings }
+) => {
+	const inputList = [...formEl.querySelectorAll(inputSelector)];
+	const buttonEl = formEl.querySelector(submitButtonSelector);
+	toggleButtonState(inputList, buttonEl, otherSettings);
+
+	inputList.forEach((inputEl) => {
+		inputEl.addEventListener("input", (e) => {
+			checkInputValidity(formEl, inputEl, otherSettings);
+			toggleButtonState(inputList, buttonEl, otherSettings);
+		});
+	});
+};
+
+const enableValidation = ({ formSelector, ...otherSettings }) => {
+	const formList = [...document.querySelectorAll(formSelector)];
+	formList.forEach((formEl) => {
+		formEl.addEventListener("submit", (evt) => {
+			evt.preventDefault();
+		});
+		setupEventListeners(formEl, otherSettings);
+	});
+};
+
+enableValidation({
+    formSelector: ".popup__form",
+    inputSelector: ".popup__input",
+    submitButtonSelector: ".popup__save-button",
+    inactiveButtonClass: "popup__save-button_type_disabled",
+    inputErrorClass: "popup__input_type_error",
+    errorClass: "popup__error_visible",
+  });
+  
