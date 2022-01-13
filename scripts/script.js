83c13999c1a1e663521104b30fb9c883fc6e5a30
Mark Worsham <mworsham@gmail.com>
2022-01-13 09:11:56 -0600

    fix: moved card data to scriptdotjs

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,35 @@ import FormValidator from "./FormValidator.js";
 import Card from "./card.js";
 import { openModalWindow, closeModalWindow } from "./utils.js";
 
+/* ------------------------------ Card array ------------------------------ */
+const initialCards = [
+  {
+    title: "Yosemite Valley",
+    image: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+  },
+  {
+    title: "Lake Louise",
+    image: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+  },
+  {
+    title: "Bald Mountains",
+    image: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+  },
+  {
+    title: "Latemar",
+    image: "https://code.s3.yandex.net/web-code/latemar.jpg"
+  },
+  {
+    title: "Vanoise National Park",
+    image: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+  },
+  {
+    title: "Lago di Braies",
+    image: "https://code.s3.yandex.net/web-code/lago.jpg"
+  }
+];
+
+
 /* -------------------------------- Wrappers -------------------------------- */
 const editModal = document.querySelector(".popup_type_edit");
 const editForm = editModal.querySelector(".popup__form");
